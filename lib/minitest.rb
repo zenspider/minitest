6d1bf264fa23ff4e7d431cd599e87f98db40c737
Ryan Davis <ryand-ruby@zenspider.com>
2018-03-12 04:56:57 -0700

    - Removed old conditional for prerecord

--- lib/minitest.rb
+++ lib/minitest.rb
@@ -1000,8 +1000,7 @@ module Minitest
 
     def prerecord klass, name # :nodoc:
       self.reporters.each do |reporter|
-        # TODO: remove conditional for minitest 6
-        reporter.prerecord klass, name if reporter.respond_to? :prerecord
+        reporter.prerecord klass, name
       end
     end
 
