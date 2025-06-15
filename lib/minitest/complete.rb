01d97e70d8af1fc9ea1a516b90517f1c89544c1a
Ryan Davis <ryand-ruby@zenspider.com>
2025-06-14 17:30:13 -0700

    + Added minitest/complete to help with shell's tab-completion of tests.

---
+++ lib/minitest/complete.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+# :stopdoc:
+
+# see instructions in the readme for use
+
+require 'optparse'
+require 'shellwords'
+
+argv = Shellwords.split(ENV['COMP_LINE']).drop 1
+options = {}
+
+begin
+  OptionParser.new do |opts|
+    opts.on("-n", "--name [METHOD]", "Test method") do |m|
+      options[:method] = m
+    end
+  end.parse!(argv)
+rescue
+  retry # ignore options passed to Ruby
+end
+
+file = argv.find { |f| File.file?(f) && !File.directory?(f) }
+
+exit unless options.key?(:method) && file
+
+require 'ripper'
+
+methods = []
+K = Class.new(Ripper) { define_method(:on_def) { |n,_,_| methods << n } }
+
+begin
+  K.parse File.read(file), file, 1
+  methods = methods.grep(/^#{options[:method]}/) if options[:method]
+  puts methods.join("\n")
+rescue # give up on parse errors
+end
+
+# :startdoc:
