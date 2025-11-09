b5ffc5b936660a3443686ac0bb83dd099ad14dea
Ryan Davis <ryand-ruby@zenspider.com>
2025-11-09 00:53:22 -0800

    + [MOCK] assert_operator and assert_predicate both call assert_respond_to first.

--- test/minitest/test_minitest_mock.rb
+++ test/minitest/test_minitest_mock.rb
@@ -676,7 +676,7 @@ class TestMinitestStub < Minitest::Test
   end
 
   def assert_stub val_or_callable
-    @assertion_count += 1
+    @assertion_count += 2
 
     t = Time.now.to_i
 
@@ -688,7 +688,7 @@ class TestMinitestStub < Minitest::Test
   end
 
   def test_stub_private_module_method
-    @assertion_count += 1
+    @assertion_count += 2
 
     t0 = Time.now
 
@@ -730,7 +730,7 @@ class TestMinitestStub < Minitest::Test
   end
 
   def test_stub_block_args
-    @assertion_count += 1
+    @assertion_count += 2
 
     t = Time.now.to_i
 
